c20f2646c0aac1f850bb9d217faa9b96442d1e75
Indosaram <freedom_zero@naver.com>
2022-09-16 00:22:23 +0900

    fix text

--- typlore/src/App.js
+++ typlore/src/App.js
@@ -1,77 +1,17 @@
-import React, { useRef, useState } from "react";
-//R3F
-import { Canvas, useFrame } from "react-three-fiber";
-// Deai - R3F
-import { softShadows, MeshWobbleMaterial, OrbitControls } from "drei";
-//Components
+import React from "react";
 import Header from "./components/Header";
-// Styles
-import "./App.scss";
-// React Spring
-import { useSpring, a } from "react-spring/three";
+import "./App.css";
 
-import { ForceGraph3D } from 'react-force-graph';
-
-
-
-
-// soft Shadows
-softShadows();
-
-// const SpinningMesh = ({ position, color, speed, args }) => {
-//   //ref to target the mesh
-//   const mesh = useRef();
-
-//   //useFrame allows us to re-render/update rotation on each frame
-//   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
-
-//   //Basic expand state
-//   const [expand, setExpand] = useState(false);
-//   // React spring expand animation
-//   const props = useSpring({
-//     scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1],
-//   });
-//   return (
-//     <a.mesh
-//       position={position}
-//       ref={mesh}
-//       onClick={() => setExpand(!expand)}
-//       scale={props.scale}
-//       castShadow
-//     >
-//       <boxBufferGeometry attach="geometry" args={args} />
-//       <MeshWobbleMaterial
-//         color={color}
-//         speed={speed}
-//         attach="material"
-//         factor={0.6}
-//       />
-//     </a.mesh>
-
-//     //Using Drei box if you want
-//     // <Box {...props} ref={mesh} castShadow>
-//     //   <MeshWobbleMaterial
-//     //     {...props}
-//     //     attach='material'
-//     //     factor={0.6}
-//     //     Speed={1}
-//     //   />
-//     // </Box>
-//   );
-// };
+import { ForceGraph3D } from "react-force-graph";
 
 const App = () => {
   const points = require("./particle_axis.json");
-  return (<>
-    <ForceGraph3D
-    graphData={points}
-    /> 
-    <Header>
-      Hello
-    </Header>
+  return (
+    <>
+      <Header>Hello</Header>
+      <ForceGraph3D graphData={points} />
     </>
   );
 };
-
 
 export default App;
--- typlore/src/components/Header.js
+++ typlore/src/components/Header.js
@@ -1,27 +1,22 @@
 import React from "react";
 
-
 const Header = () => {
-  return (<>
-    <header>
-      <div className='project'>
-        <h1>2022 Gradeshow
-        </h1>
-        
-      </div>
-      <div className='episode'>
+  return (
+    <>
+      <h1 className="project">2022 Gradeshow</h1>
+      {/* <div className="episode">
         <span>Typlore</span>
         <div>Explore your typo neuron</div>
       </div>
-      <div class="main_container">
-        <div data-view="pinned_top" 
-          data-set="Scaffolding" 
-          data-position="top" 
-          class="pinned pinned_top">
-          </div>
-      </div>
-    </header>
-  </>    
+      <div className="main_container">
+        <div
+          data-view="pinned_top"
+          data-set="Scaffolding"
+          data-position="top"
+          className="pinned pinned_top"
+        ></div>
+      </div> */}
+    </>
   );
 };
 
